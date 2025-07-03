2324286ae4d506be2f48e36b405e86ba61910f9f
mbportz <mbportuguez2@gmail.com>
2025-07-03 16:04:12 +0800

    code clean up

--- weather-app/webpack.prod.js
+++ weather-app/webpack.prod.js
@@ -30,7 +30,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const Dotenv = require("dotenv-webpack");
 const webpack = require("webpack");
 
 module.exports = {
